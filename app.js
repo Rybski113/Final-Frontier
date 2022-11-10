a810c38e0a2e3b34c7099b69327c1e76649a1fdb
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-11-10 08:04:45 +0100

    Array and shooter
    I've made an array that holds all the divs in the grid and also added a shooter

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const grid = document.getElementById('grid');
+let currentShooterIndex = 205;
 
 
-for (let i = 0; i < 900; i++) {
+for (let i = 0; i < 225; i++) {
     const square = document.createElement('div');
     grid.appendChild(square);
 }
+
+const squares = Array.from(document.querySelectorAll('#grid div'));
+
+squares[currentShooterIndex].classList.add('shooter')
