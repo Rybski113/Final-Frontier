5e04f7232b9a4178db1f274e64d179e270045267
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-11-23 11:17:17 +0100

    grid ans enemies setup

---
+++ EnemyController.js
@@ -0,0 +1,26 @@
+export default class EnemyController {
+    enemyMap = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [2, 2, 2, 3, 3, 3, 3, 2, 2, 2],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+      ];
+
+      enemyRows = [];
+
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.createEnemies();
+    }
+
+    draw(ctx) {}
+
+    createEnemies() {
+        this.enemyMap.forEach((row, rowIndex)=> {
+            this.enemyRows[row] = [];
+            row.forEach((enemyNumber)=> {})
+        })
+    }
+}
